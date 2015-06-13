00985653a1f500905ca189ddaf71d00b44ab22ab
MIke Carter <mike@mcarter.me>
2015-06-13 20:17:28 +0100

    Added shake animation to email input

--- code/scripts/app/main.app.js
+++ code/scripts/app/main.app.js
@@ -2,6 +2,7 @@ window.onload = function() {
 
   var snippetEnvelope = document.getElementById('snippet-envelope');
   var snippetGenerator = document.getElementById('snippet-generator');
+  var snippetGeneratorInputContainer = document.getElementById('snippet-generator-inputs');
   var snippetDisplay = document.getElementById('snippet-display'); 
 
   var snippetGeneratorEmail = document.getElementById('snippet-generator-email');
@@ -11,14 +12,20 @@ window.onload = function() {
   snippetGeneratorButton.addEventListener('click', function() {
 
     if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(snippetGeneratorEmail.value)) {
-      return alert("Please enter a valid email address");
+      snippetGeneratorButton.blur(); // Prevents a bug in Chrome where animations don't work when button focussed.
+      snippetGeneratorInputContainer.className += " shake error";
+      setTimeout(function() {
+        snippetGeneratorInputContainer.className = snippetGeneratorInputContainer.className.replace(" shake error", "");
+
+      }, 1000);
+      return;
     }
 
     var base64Email = window.btoa(unescape(encodeURIComponent(snippetGeneratorEmail.value)));
 
     snippetDisplayEmail.innerHTML = base64Email;
 
-    snippetEnvelope.className += " " + "bounceOutDown";
+    snippetEnvelope.className += " bounceOutDown";
 
     setTimeout(function() {
       snippetGenerator.style.display = "none";
