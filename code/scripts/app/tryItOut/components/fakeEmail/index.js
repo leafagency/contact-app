7db78d90379ab73215c041f65c18fb1b7000b970
Mike Carter <mike@weareleaf.com>
2016-12-02 09:48:23 +0000

    Add a simple affirmitive shake to the email when it updates

--- code/scripts/app/tryItOut/components/fakeEmail/index.js
+++ code/scripts/app/tryItOut/components/fakeEmail/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import messageHelper from '../../helpers/messageHelper';
 
 class FakeEmail extends React.Component {
+  componentDidUpdate() {
+    const classList = document.getElementById('fake-email').classList
+    classList.add('shake-vertical')
+    setTimeout(() => classList.remove('shake-vertical'), 1000)
+  }
+
   render() {
-    const subject = messageHelper.buildSubject(this.props.emailData)
-    const fromText = messageHelper.buildFromText(this.props.emailData)
-    const message = messageHelper.buildMessage(this.props.emailData)
-    const replyTo = this.props.email || 'no-reply@howdyform.com'
+    const { data } = this.props
+    const subject = messageHelper.buildSubject(data)
+    const fromText = messageHelper.buildFromText(data)
+    const message = messageHelper.buildMessage(data)
+    const replyTo = data.email || 'no-reply@howdyform.com'
 
     return (
       <div className='fake-email-container'>
-        <div className='fake-email'>
+        <div className='fake-email animated--short' id="fake-email">
           <div className="fake-email__from">
             {fromText}
           </div>
