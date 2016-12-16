bf75a53ec6ba7959ec4673fbaf99d9ed8d48169b
Mike Carter <mike@weareleaf.com>
2016-12-16 10:46:55 +0000

    Update with new snippet JS URL

--- code/scripts/app/config.js
+++ code/scripts/app/config.js
@@ -6,11 +6,16 @@ var buildApiUrl = function(endpoint) {
   return config.protocol + '://' + config.apiHost + endpoint;
 }
 
+var buildAssetUrl = function(file) {
+  return config.protocol + '://' + config.assetHost + file;
+}
+
 config.protocol = inDevelopment ? 'http' : 'https'
 config.apiHost = inDevelopment ? 'localhost:8080' : 'app.howdyform.com'
+config.assetHost = inDevelopment ? 'static.howdyform.com' : 'static.howdyform.com'
 config.stripeKey = inDevelopment ? 'pk_test_6MCiAEXja5maiISN7W3cXjhP' : 'pk_live_COqrme9FBvt1ssbwvxEomd2I';
 
 config.verifyRecipientUrl = buildApiUrl('/api/recipients/verify')
 config.createAccountUrl = buildApiUrl('/api/accounts')
-config.libraryUrl = buildApiUrl('/static/howdyClient.js')
+config.libraryUrl = buildAssetUrl('/howdyClient.js')
 config.createSubscriptionUrl = buildApiUrl('/api/subscriptions')
