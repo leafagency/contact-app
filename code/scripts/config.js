267e5a5d6bcb56d1fabc9a16b3c8de019e3553e4
Mike Carter <mike@mcarter.me>
2017-09-10 10:15:27 +0100

    Add staging support

--- code/scripts/config.js
+++ code/scripts/config.js
@@ -1,6 +1,6 @@
-var config = module.exports
-
-var inDevelopment = window.location.port && window.location.hostname.match(/local/);
+var inStaging = window.location.search.match(/staging=/)
+var inDevelopment = window.location.port && window.location.hostname.match(/local/) && !inStaging
+var inProduction = !inStaging && !inDevelopment
 
 var buildApiUrl = function(endpoint) {
   return config.protocol + '://' + config.apiHost + endpoint;
@@ -10,11 +10,11 @@ var buildAssetUrl = function(file) {
   return config.protocol + '://' + config.assetHost + file;
 }
 
+var config = module.exports
 config.protocol = inDevelopment ? 'http' : 'https'
-config.apiHost = inDevelopment ? 'localhost:8080' : 'app.howdyform.com'
+config.apiHost = inDevelopment ? 'localhost:8080' : (inStaging ? 'app-staging.howdyform.com' : 'app.howdyform.com')
 config.assetHost = inDevelopment ? 'static.howdyform.com' : 'static.howdyform.com'
-config.stripeKey = inDevelopment ? 'pk_test_6MCiAEXja5maiISN7W3cXjhP' : 'pk_live_COqrme9FBvt1ssbwvxEomd2I';
-
+config.stripeKey = (inDevelopment || inStaging) ? 'pk_test_6MCiAEXja5maiISN7W3cXjhP' : 'pk_live_COqrme9FBvt1ssbwvxEomd2I';
 config.verifyRecipientUrl = buildApiUrl('/api/recipients/verify')
 config.createAccountUrl = buildApiUrl('/api/accounts')
 config.libraryUrl = buildAssetUrl('/howdyClient.js')
